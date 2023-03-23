115b8bc7b927105cf3b4dc649d296c95073b8899
ebarcott <berika@netapp.com>
2023-03-22 17:30:34 -0700

    doot doot

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://erikabarcott.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-test/',
+  baseUrl: '/docusaurus-test/build',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
